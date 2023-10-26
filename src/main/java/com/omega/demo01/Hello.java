7d6be09071a01f5fab027e9f0875634a9aff9e41
Kenny82005570 <Kenny.HX.Su@pccw.com>
2023-10-26 12:06:45 +0800

    Chapter01  Managed Beans.

---
+++ src/main/java/com/omega/demo01/Hello.java
@@ -0,0 +1,43 @@
+package com.omega.demo01;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Class HelloWorld
+ *
+ * @author KennySu
+ * @date 2023/10/26
+ */
+@ManagedBean
+@RequestScoped
+public class Hello {
+
+    /**
+     * 依赖注入时,必须要@ManagedProperty注解和setter方法,不需要getter方法。
+     */
+    @ManagedProperty(value = "#{message}")
+    private Message message;
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    /**
+     * Tip:不能重写getter方法作为q前端拿消息的方法, 否则会报注入message对象失败的错误
+     */
+    /*public String getMessage() {
+        if(message != null) {
+            return message.getMessage();
+        }
+        return "";
+    }*/
+
+    public String getSayHello() {
+        if(message != null) {
+            return message.getMessage();
+        }
+        return "";
+    }
+}
---
+++ src/main/java/com/omega/demo01/Message.java
@@ -0,0 +1,25 @@
+package com.omega.demo01;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Class Message
+ *
+ * @author KennySu
+ * @date 2023/10/26
+ */
+@ManagedBean
+@RequestScoped
+public class Message {
+
+    private String message = "Hello World!";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
