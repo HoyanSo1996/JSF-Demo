eb2d246afce1563d73cb836ea11e527f6c0e3f43
Kenny82005570 <Kenny.HX.Su@pccw.com>
2023-10-26 10:53:57 +0800

    Initialization.

---
+++ README.md
@@ -0,0 +1,5 @@
+1. JSF教程
+   https://iowiki.com/jsf/jsf_index.html
+
+2. PrimeFaces教程
+   https://www.primefaces.org/showcase/index.xhtml?jfwid=311e8
---
+++ src/main/java/com/omega/demo/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.omega.demo;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Class HelloWorld
+ *
+ * @author KennySu
+ * @date 2023/10/26
+ */
+@ManagedBean(eager = true)
+public class HelloWorld {
+
+    public HelloWorld() {
+        System.out.println("JSF started!");
+    }
+
+    public String getMessage() {
+        return "Hello, JSF!";
+    }
+}
